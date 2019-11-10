578ff5600a552d7d85a2740f4414e9e06e8ce7f9
Ariniinf <arini.inf@gmail.com>
2019-11-10 23:15:50 +0700

    login siji

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -6,45 +6,44 @@ class Login extends CI_Controller {
 	}
 	public function login_validation(){
 		$this->load->library('form_validation');
-		$this->form_validation->set_rules('username','Username',['required'=> 'Username wajib diisi!']);
-		$this->form_validation->set_rules('password','Password',['required'=> 'Password wajib diisi!']);
-		if($this->form_validation->run()== FALSE){ 
-            $this->load->view('donatur/view_logindonatur');}
-            
-            else{
+		$this->form_validation->set_rules('username','Username','required');
+		$this->form_validation->set_rules('password','Password','required');
+		if($this->form_validation->run())
+		{
 			$username=$this->input->post('username');
-            $password=$this->input->post('password');
-            
-            $user =$username;
-            $pass =MD5($password);
-
-            $cek= $this->model_login->cek_login($user, $pass);
-			//$this->load->model('model_login');
-
-			if($cek -> num_rows() >0)
+			$password=$this->input->post('password');
+			$pass = md5($password);
+			$this->load->model('model_login');
+			if($this->model_login->can_login($username,$pass))
 			{
-                    foreach ($cek -> result() as $ck){
-                        $sess_data['username'] = $ck->username;
-                        $sess_data['email'] = $ck->email;
-
-
-                        $this ->session->set_userdata($sess_data);
-                    }
-				
-			    redirect('donatur/dashboard');
-			    }
+				$session_data = array(
+					'username' => $username,
+					'password' => md5($password),
+					'status' => "sudahlogin"
+					);
+				$this->session->set_userdata($session_data);
+				redirect(base_url() . 'login/enter');
+			}
 			else{
-                $this->session->set_flashdata('pesan','<div class="alert alert-danger alert-dismissible fade show" role="alert">
-                Username atau Password salah!
-               <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                 <span aria-hidden="true">&times;</span>
-               </button>
-             </div>');
-               redirect('login');
-			    }
-		    }
-    
+				$this->session->set_flashdata('error','Invalid Username or Password');
+				redirect(base_url() . 'login/index');
+			}
+		}
+		else{
+			$this->index();
+		}
+
 	}
+	
+	public function enter(){
+  		$username = $this->session->userdata('username');
+   		if($username==""){
+   		 redirect('login/index');
+   		}
+   		else{
+    		redirect('donatur/dashboard');
+  		}
+ 	}
 	
 	public function logout(){
 		$this->session->sess_destroy(); 
@@ -54,3 +53,4 @@ class Login extends CI_Controller {
 }
 
 ?>
+
--- application/models/model_login.php
+++ application/models/model_login.php
@@ -1,40 +1,53 @@
 <?php
-class Login_model extends CI_Model{
-    public function cek_login($username, $password){
-        $this->db->where("username", $username);
-        $this->db->where("password", $password);
-        //konek -db nya
-        return $this->db->get('bantupanti.donatur');
+class Model_login extends CI_Model{
+    // public function cek_login($username, $password){
+    //     $this->db->where("username", $username);
+    //     $this->db->where("password", $password);
+    //     //konek -db nya
+    //     return $this->db->get('bantupanti.donatur');
 
+	// }
+	
+	function can_login($username,$password)
+    {
+      $this->db->where('username',$username);
+      $this->db->where('password',$password);
+      $query = $this->db->get('bantupanti.donatur');
+      if($query->num_rows() > 0){
+        return true;
+      }
+      else{
+        return false;
+      }
     }
-    public function getLoginData($user, $pass){
-        $u = $user;
-        $p = MD5($pass);
+    // public function getLoginData($user, $pass){
+    //     $u = $user;
+    //     $p = MD5($pass);
 
-        $query_cekLogin = $this->db->get_where('user', array('username' =>$su, 'password'=>$p));
+    //     $query_cekLogin = $this->db->get_where('user', array('username' =>$su, 'password'=>$p));
 
-        if(count($query_cekLogin->result())>0){
-            foreach ($query_cekLogin->result() as $qck){
-                foreach ($query_cekLogin->result() as $ck){
-                    $sess_data ['logged_in'] = TRUE;
-                    $sess_data ['username'] = $ck->username;
-                    $sess_data ['password'] = $ck->password;
-                    $this->session->set_userdata($sess_data);
-                }
-                redirect('donatur/view_logindonatur');
-            } 
+    //     if(count($query_cekLogin->result())>0){
+    //         foreach ($query_cekLogin->result() as $qck){
+    //             foreach ($query_cekLogin->result() as $ck){
+    //                 $sess_data ['logged_in'] = TRUE;
+    //                 $sess_data ['username'] = $ck->username;
+    //                 $sess_data ['password'] = $ck->password;
+    //                 $this->session->set_userdata($sess_data);
+    //             }
+    //             redirect('donatur/view_logindonatur');
+    //         } 
 
              
-        } else{
-            $this->session->set_flashdata('pesan','<div class="alert alert-danger alert-dismissible fade show" role="alert">
-            Username atau Password salah!
-           <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-             <span aria-hidden="true">&times;</span>
-           </button>
-         </div>');
-            redirect('donatur');
-        }
-    }
+    //     } else{
+    //         $this->session->set_flashdata('pesan','<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    //         Username atau Password salah!
+    //        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    //          <span aria-hidden="true">&times;</span>
+    //        </button>
+    //      </div>');
+    //         redirect('donatur');
+    //     }
+    // }
 }
 
 ?>
