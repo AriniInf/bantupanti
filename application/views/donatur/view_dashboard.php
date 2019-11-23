313fcbf6263af7f2b89ed201213c9c3eacf6e41c
awin-cute <awinawin0014@gmail.com>
2019-11-24 00:43:32 +0700

    view-dashboarddonatur

--- application/views/donatur/view_dashboard.php
+++ application/views/donatur/view_dashboard.php
@@ -29,69 +29,142 @@
         
           <div class="box-body">
           
-          <center><h3>Langkah-langkah berdonasi</h3></center>
+          <center><h3><b>Langkah-langkah berdonasi</b></h3></center>
            
            <br>
             
-            <div class="card">
+            <div class="card1">
             
              
               <img src="<?php echo base_url('assets/image/one.png')?>" alt="Avatar" style="width:100%">
               <div class="container">
                 <!-- <h4><b>John Doe</b></h4>  -->
-                <p><b>Login sebagai donatur</b></p> 
+                
               </div>
+              
             </div>
 
+            <div class="arrow">
+            
+             
+            <img src="<?php echo base_url('assets/image/right-arrow.png')?>" alt="Avatar" style="width:100%">
+            <div class="container">
+              <!-- <h4><b>John Doe</b></h4>  -->
+              
+            </div>
+            
+          </div>
+          
             <div class="card">
              
              <img src="<?php echo base_url('assets/image/two.png')?>" alt="Avatar" style="width:100%">
              <div class="container">
                <!-- <h4><b>John Doe</b></h4>  -->
-               <p><b>Klik menu donasi</b></p> 
+               
              </div>
-           </div>
+           </div>           
+           <div class="arrow">
+            
+             
+            <img src="<?php echo base_url('assets/image/right-arrow.png')?>" alt="Avatar" style="width:100%">
+            <div class="container">
+              <!-- <h4><b>John Doe</b></h4>  -->
+              
+            </div>
+            </div>
+
 
            <div class="card">
              
              <img src="<?php echo base_url('assets/image/three.png')?>" alt="Avatar" style="width:100%">
              <div class="container">
                <!-- <h4><b>John Doe</b></h4>  -->
-               <p><b>Klik  sub-menu berdonasi</b></p> 
+               
              </div>
            </div>
+
+           <div class="arrow">
+            
+             
+            <img src="<?php echo base_url('assets/image/right-arrow.png')?>" alt="Avatar" style="width:100%">
+            <div class="container">
+              <!-- <h4><b>John Doe</b></h4>  -->
+              
+            </div>
+            </div>
 
            <div class="card">
              
              <img src="<?php echo base_url('assets/image/four.png')?>" alt="Avatar" style="width:100%">
              <div class="container">
                <!-- <h4><b>John Doe</b></h4>  -->
-               <p><b>Isi dan upload bukti tf</b></p> 
+               
              </div>
            </div>
+           <div class="arrow">
+            
+             
+            <img src="<?php echo base_url('assets/image/right-arrow.png')?>" alt="Avatar" style="width:100%">
+            <div class="container">
+              <!-- <h4><b>John Doe</b></h4>  -->
+              
+            </div>
+            </div>
 
            <div class="card">
              
              <img src="<?php echo base_url('assets/image/five.png')?>" alt="Avatar" style="width:100%">
              <div class="container">
                <!-- <h4><b>John Doe</b></h4>  -->
-               <p><b>klik tombol donasi</b></p> 
+               
              </div>
            </div>
+           <div class="arrow">
+            
+             
+            <img src="<?php echo base_url('assets/image/right-arrow.png')?>" alt="Avatar" style="width:100%">
+            <div class="container">
+              <!-- <h4><b>John Doe</b></h4>  -->
+              
+            </div>
+            </div>
 
-           <div class="card">
+           <div class="card6">
              
              <img src="<?php echo base_url('assets/image/six.png')?>" alt="Avatar" style="width:100%">
              <div class="container">
                <!-- <h4><b>John Doe</b></h4>  -->
-               <p><b>Donasi Anda, akan diproses</b></p> 
+               
              </div>
+             
            </div>
+          <div class="box-body">
+            <div class="tulisan1">
+            <h5><b>Login sebagai donatur</b></h5> 
+            </div>
+            <div class="tulisan2">
+            <h5><b>Klik menu donasi</b></h5>
+            </div>
+            <div class="tulisan3">
+            <h5><b>Klik  sub-menu berdonasi</b></h5> 
+            </div>
+            <div class="tulisan4">
+            <h5><b>Isi dan upload bukti tf</b></h5> 
+            </div>
+
+            <div class="tulisan5">
+            <h5><b>klik tombol donasi</b></h5>
+            </div>
+            <div class="tulisan6">
+             
+            <h5><b>Donasi Anda, akan diproses</b></h5> 
+            </div>
+          </div>
 
           </div>
             <div class="box-body">
               <div class="don">
-                <img src="<?php echo base_url('assets/image/carousel12.jpg')?>" style="width:100%">
+                <img src="<?php echo base_url('assets/image/nate.jpg')?>" style="width:100%">
              </div>
             </div>
       </div>
@@ -117,15 +190,92 @@
   .card {
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
   transition: 0.3s;
+  width: 5%;
+  float: left;
+  border: 20%;
+
+}
+.card1 {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 5%;
+  float: left;
+  border: 20%;
+  margin-left: 20px;
+
+}
+.card6 {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 5%;
+  float: left;
+  border: 20%;
+  margin-right: 40px;
+
+}
+.arrow{
+  width: 5%;
+  float: left;
+  border: 20%;
+  margin-left: 47px;
+  margin-right: 47px;
+
+}
+.tulisan{
   width: 15%;
   float: left;
-  border: 5px;
+  border: 20%;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+.tulisan1{
+  width: 15%;
+  float: left;
+  border: 0%;
+  margin-left: 0px;
+  margin-right: 12px;
+}
+.tulisan2{
+  width: 15%;
+  float: left;
+  border: 0%;
+  margin-left: 3px;
+  margin-right: 10px;
+}
+.tulisan3{
+  width: 15%;
+  float: left;
+  border: 0%;
+  margin-left: 7px;
+  margin-right: 15px;
+}
+.tulisan4{
+  width: 15%;
+  float: left;
+  border: 0%;
+  margin-left: 20px;
+  margin-right: 25px;
+}
+.tulisan5{
+  width: 12%;
+  float: left;
+  border: 0%;
+  margin-left: 22px;
   margin-right: 20px;
 }
-
+.tulisan6{
+  width: 15%;
+  float: left;
+  border: 0%;
+  margin-left: 20px;
+  margin-right: 0px;
+}
 .card:hover {
   box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
 }
+/* .box-body{
+ background-color: #ff383b;
+} */
 
 .container {
   padding: 2px 16px;
@@ -151,4 +301,16 @@
     		background-image: url("<?php echo base_url(''); ?>assets/img/STPN2.jpg");
     	}
     } */
+    @font-family{
+      font-family: 'letters';
+      src: url("<?php echo base_url('assets/fonts/letters.ttf'); ?>
+    }
+    
+    h3{
+      font-family: letters;
+    }
+  
+    
+ 
+  
   </style>
